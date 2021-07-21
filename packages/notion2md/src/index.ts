a7f5d373e0a19ea6455553286907485737188165
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-20 20:12:10 -0700

    perf: ini yargs parser

--- packages/notion2md/src/index.ts
+++ packages/notion2md/src/index.ts
@@ -1,3 +1,19 @@
-console.log("This is a TypeScript file.");
+import { Client } from "@notionhq/client";
+import yargs from "yargs";
+
+const parser = yargs.options({
+  api: { type: "string", demandOption: true, alias: "a" },
+});
+
+const notion = new Client({
+  auth: process.env.NOTION_TOKEN,
+});
+
+export const getPage = async (pageId: string) => {
+  const response = await notion.pages.retrieve({ page_id: pageId });
+  return response;
+};
+
+console.log(`Running test on ${parser.parseSync().api}`);
 
 export {};
