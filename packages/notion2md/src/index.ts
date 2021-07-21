b2b8ae3500e901026dc0630eef14d95f9ac5dc75
Shun Kakinoki <shunkakinoki@gmail.com>
2021-07-20 18:41:35 -0700

    perf: ini node tsc

--- packages/notion2md/src/index.ts
+++ packages/notion2md/src/index.ts
@@ -1,5 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-const main = () => {
-  // eslint-disable-next-line no-console
-  console.log("This is a TypeScript file.");
-};
+console.log("This is a TypeScript file.");
+
+export {};
