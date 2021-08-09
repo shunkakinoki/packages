69ad66ebfae66a5266090905414038ee8b5ded1c
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-08 19:08:08 -0700

    feat: revamp named exports

--- packages/next-og-utils/src/index.ts
+++ packages/next-og-utils/src/index.ts
@@ -1,7 +1,18 @@
-export * from "./const";
-export * from "./css";
-export * from "./getHtml";
-export * from "./getScreenshot";
-export * from "./getTheme";
-export * from "./sanitizeHtml";
-export * from "./types";
+export { OG_WIDTH, OG_HEIGHT } from "./const";
+export { css, getCommonCSS } from "./css";
+export { getHtml } from "./getHtml";
+export { getScreenshot } from "./getScreenshot";
+export { getTheme } from "./getTheme";
+export { sanitizeHtml } from "./sanitizeHtml";
+export type {
+  FileType,
+  IConfig,
+  BaseLayoutProperty,
+  ILayoutValue,
+  ILayoutConfig,
+  LayoutComponent,
+  Style,
+  ILayoutProperty,
+  GetCSSFn,
+  ILayout,
+} from "./types";
