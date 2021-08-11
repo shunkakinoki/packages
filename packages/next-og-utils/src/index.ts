8485732fc9a6b5ce285d9401b4647a3908aa1468
Shun Kakinoki <shunkakinoki@gmail.com>
2021-08-11 00:11:32 -0700

    chore: update

--- packages/next-og-utils/src/index.ts
+++ packages/next-og-utils/src/index.ts
@@ -1,7 +1,6 @@
 export { OG_WIDTH, OG_HEIGHT } from "./const";
 export { css, getCommonCSS } from "./css";
 export { getHtml } from "./getHtml";
-export { getScreenshot } from "./getScreenshot";
 export { getTheme } from "./getTheme";
 export { sanitizeHtml } from "./sanitizeHtml";
 export type {
